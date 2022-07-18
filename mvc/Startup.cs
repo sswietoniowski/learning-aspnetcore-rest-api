2274560332e467bb4ca248a72f0f4e85398b4ba1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-07-18 14:00:34 +0200

    Cleaned up the starter code

--- mvc/Startup.cs
+++ mvc/Startup.cs
@@ -23,7 +23,6 @@ namespace mvc
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -34,7 +33,6 @@ namespace mvc
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
