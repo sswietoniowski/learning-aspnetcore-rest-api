ab0550cebb50ea3e7e691891e6e8baa7231a7f5f
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-06 08:15:36 +0200

    Optimized repostiory for the minimal API

--- minimal/DataAccess/Repository/Repository.cs
+++ minimal/DataAccess/Repository/Repository.cs
@@ -41,7 +41,8 @@ namespace minimal.DataAccess.Repository
                 query = orderBy(query);
             }
 
-            return await query.AsNoTracking().ToListAsync();
+            var result = await query.AsNoTracking().ToListAsync();
+            return result.AsReadOnly();
         }
 
         public async Task<T?> GetAsync(int id)
@@ -68,7 +69,7 @@ namespace minimal.DataAccess.Repository
                         current.Include(includeProperty));
             }
 
-            return await query.FirstOrDefaultAsync();
+            return await query.AsNoTracking().FirstOrDefaultAsync();
         }
 
         public async Task AddAsync(T entity)
