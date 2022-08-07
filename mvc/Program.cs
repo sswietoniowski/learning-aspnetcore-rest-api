df48a072b737d61bb54925e8b10ad97540172a7d
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-08-07 08:18:08 +0200

    Refactored Program class to use top level statements Also removed Startup class and its content approprietly moved to Program.

--- mvc/Program.cs
+++ mvc/Program.cs
@@ -1,17 +1,57 @@
-namespace mvc
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using mvc.DataAccess.Data;
+using mvc.DataAccess.Repository;
+using mvc.DataAccess.Repository.Interfaces;
+using Newtonsoft.Json.Serialization;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<QuotesDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("QuotesDb"));
+});
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+})
+    .AddNewtonsoftJson(settings =>
+    {
+        settings.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        settings.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    })
+    .AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "mvc", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "mvc v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
