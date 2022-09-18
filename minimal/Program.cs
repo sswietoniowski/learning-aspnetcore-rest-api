2af5024870713bdee16de10802deb9beb81fc975
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-18 11:08:18 +0200

    Extracted minimal API endpoints to a dedicated extension file

--- minimal/Program.cs
+++ minimal/Program.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 
 using Microsoft.EntityFrameworkCore;
 
+using minimal;
 using minimal.DataAccess.Data;
 using minimal.DataAccess.Entities;
 using minimal.DataAccess.Repository;
@@ -59,70 +60,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("api/quotes", async (IUnitOfWork unitOfWork, IMapper mapper) =>
-{
-    var (quotes, _) = await unitOfWork.QuoteRepository.GetQuotesAsync();
-    var quotesDto = mapper.Map<IEnumerable<QuoteDto>>(quotes);
-
-    return Results.Ok(quotesDto);
-});
-
-app.MapGet("api/quotes/{id}", async (int id, IUnitOfWork unitOfWork, IMapper mapper) =>
-{
-    var quote = await unitOfWork.QuoteRepository.GetByIdAsync(id);
-
-    if (quote is null)
-    {
-        return Results.NotFound();
-    }
-
-    var quoteDto = mapper.Map<QuoteDto>(quote);
-
-    return Results.Ok(quoteDto);
-});
-
-app.MapPost("api/quotes", async (QuoteForCreationDto quoteDto, IUnitOfWork unitOfWork, IMapper mapper) =>
-{
-    var quote = mapper.Map<QuoteEntity>(quoteDto);
-
-    await unitOfWork.QuoteRepository.AddAsync(quote);
-    await unitOfWork.SaveAsync();
-
-    var createdQuoteDto = mapper.Map<QuoteDto>(quote);
-
-    return Results.Created($"/api/quotes/{createdQuoteDto.Id}", createdQuoteDto);
-});
-
-app.MapPut("api/quotes/{id}", async (int id, QuoteForUpdateDto quoteDto, IUnitOfWork unitOfWork, IMapper mapper) =>
-{
-    var quoteToUpdate = await unitOfWork.QuoteRepository.GetByIdAsync(id);
-
-    if (quoteToUpdate is null)
-    {
-        return Results.NotFound();
-    }
-
-    mapper.Map(quoteDto, quoteToUpdate);
-
-    unitOfWork.QuoteRepository.Modify(quoteToUpdate);
-    await unitOfWork.SaveAsync();
-
-    return Results.NoContent();
-});
-
-app.MapDelete("api/quotes/{id}", async (int id, IUnitOfWork unitOfWork) =>
-{
-    var quoteToDelete = await unitOfWork.QuoteRepository.GetByIdAsync(id);
-
-    if (quoteToDelete is null)
-    {
-        return Results.NotFound();
-    }
-
-    unitOfWork.QuoteRepository.Remove(quoteToDelete);
-    await unitOfWork.SaveAsync();
-
-    return Results.NoContent();
-});
+app.MapQuoteEndpoints();
 
 app.Run();
