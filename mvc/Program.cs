e6c64442f2ba1625777737cb59e1da692f6b2762
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-11-26 21:00:50 +0100

    Final refactor

--- mvc/Program.cs
+++ mvc/Program.cs
@@ -1,31 +1,20 @@
 using mvc.Configurations.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.ConfigureLogging();
-
-builder.ConfigurePersistence();
-
-builder.ConfigureAutoMapper();
-
-builder.ConfigureControllers();
-
-builder.ConfigureSwagger();
-
-builder.ConfigureCors();
-
-builder.ConfigureGlobalErrorHandler();
+var builder = WebApplication.CreateBuilder(args)
+    .ConfigureLogging()
+    .ConfigurePersistence()
+    .ConfigureAutoMapper()
+    .ConfigureControllers()
+    .ConfigureSwagger()
+    .ConfigureCors()
+    .ConfigureGlobalErrorHandler();
 
 var app = builder.Build();
 
 app.UseGlobalErrorHandler();
-
 app.UseSwagger();
-
 app.UseRouting();
-
 app.UseCors();
-
 app.UseEndpoints();
 
 app.Run();
