b0e9ce33ba6ee2fc6f7197b08f361b9ab70287a7
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-11-26 10:41:43 +0100

    Added the most generic exception classes

---
+++ mvc/Exceptions/KeyNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace mvc.Exceptions;
+
+public class KeyNotFoundException : Exception
+{
+    public KeyNotFoundException(string message) : base(message)
+    {
+    }
+}
---
+++ mvc/Exceptions/NotImplementedException.cs
@@ -0,0 +1,8 @@
+﻿namespace mvc.Exceptions;
+
+public class NotImplementedException : Exception
+{
+    public NotImplementedException(string message) : base(message)
+    {
+    }
+}
